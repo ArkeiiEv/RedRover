7b61d3fe82bdac6d9b5d19b22fd806841700a76f
Alexander Kirei <alexkirei24@outlook.com>
2024-08-21 13:17:42 +0300

    add: 3 task after lesson 1

--- Java/Lesson1/src/Task1.java
+++ Java/Lesson1/src/Task1.java
@@ -1,6 +1,6 @@
 public class Task1 {
     public static void main(String[] args) {
-        for (int i = 3; i >= 0; i--) {
+        for (int i = 3; i >= 0; i--){
             if(i == 0){
                 System.out.println("Поехали!!!");
             }
