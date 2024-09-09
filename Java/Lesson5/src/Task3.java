1f1a806bed0e4c9cb40a7ce39ceaa44464490a1b
Alexander Kirei <alexkirei24@outlook.com>
2024-09-09 10:33:29 +0300

    add: All tasks from exercise 5.3

---
+++ Java/Lesson5/src/Task3.java
@@ -0,0 +1,45 @@
+public class Task3 {
+    public static void main(String[] args) {
+        printFigure1();
+        System.out.println();
+        printFigure2();
+        System.out.println();
+        printFigure3();
+    }
+
+    public static void printFigure1() {
+        for (int i = 9; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printFigure2() {
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <= 9 - i; k++) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printFigure3() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("   ");
+            }
+            for (int k = 9 - i; k >= 0; k--) {
+                System.out.print(k + " ");
+            }
+            for (int l = 0; l <= 10 - i; l++) {
+                System.out.print(l + " ");
+            }
+            System.out.println();
+        }
+    }
+}
