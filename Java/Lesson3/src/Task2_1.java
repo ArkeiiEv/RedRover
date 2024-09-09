bdbbd5bf89be369c8075694e92285f4a09a297c5
Alexander Kirei <alexkirei24@outlook.com>
2024-09-09 08:08:21 +0300

    add: Task 3.2.1

---
+++ Java/Lesson3/src/Task2_1.java
@@ -0,0 +1,9 @@
+public class Task2_1 {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a <= 15){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
