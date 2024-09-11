44fcde4835c3fb51cfbc720c4c10b4bbce40604e
Alexander Kirei <alexkirei24@outlook.com>
2024-09-11 09:57:10 +0300

    add: Home work 7

---
+++ Java/Lesson7/src/Task.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Task {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("Summary = " + sumNumbers(a, b));
+        System.out.println("Subtraction = " + subtractNumbers(a, b));
+        System.out.println("Multiplication = " + multiplyNumbers(a, b));
+        System.out.println("Division = " + divideNumbers(a, b));
+    }
+
+    public static int sumNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtractNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplyNumbers(int a, int b) {
+        return a * b;
+    }
+
+    public static int divideNumbers(int a, int b) {
+        return a / b;
+    }
+}
+
