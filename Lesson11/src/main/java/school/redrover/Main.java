cf5a44bce2e61bc752d0b1b7fb37aab2f54618dc
arkeiiEv <alexkirei24@outlook.com>
2024-10-03 22:14:42 +0300

    fix: Task 11

---
+++ Lesson11/src/main/java/school/redrover/Main.java
@@ -0,0 +1,39 @@
+package school.redrover;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(abbrevitations("Привет всем кто живет на луне", 10));
+        int a = 5;
+        int b = 10;
+        int key = a < b ? a : b;
+
+    }
+
+    public static double addNumbers(double a, double b){
+        return a + b;
+    }
+
+    public static double subNumbers(double a, double b){
+        return a - b ;
+    }
+
+    public static double multNumbers(double a, double b){
+        return a * b;
+    }
+
+    public static double divNumbers(double a, double b){
+        if(b != 0){
+            return a / b;
+        } else {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+    }
+
+    public static String abbrevitations(String str, int maxWidth){
+        String result = StringUtils.abbreviate(str, maxWidth);
+        return result;
+    }
+}
